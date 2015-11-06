2194022c285bbb89f698b41c83bec6962dd3e734
kenjiuno <h8mastre@gmail.com>
2015-11-06 18:28:07 +0900

    0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PrintBarCode2 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.bPrint = new System.Windows.Forms.Button();
+            this.bTIF = new System.Windows.Forms.Button();
+            this.bPDF = new System.Windows.Forms.Button();
+            this.cbb = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "バーコード番号 (両端*なし)：";
+            // 
+            // tb
+            // 
+            this.tb.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tb.Location = new System.Drawing.Point(12, 33);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(249, 28);
+            this.tb.TabIndex = 1;
+            // 
+            // bPrint
+            // 
+            this.bPrint.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bPrint.Location = new System.Drawing.Point(12, 123);
+            this.bPrint.Name = "bPrint";
+            this.bPrint.Size = new System.Drawing.Size(150, 46);
+            this.bPrint.TabIndex = 3;
+            this.bPrint.Text = "印刷";
+            this.bPrint.UseVisualStyleBackColor = true;
+            this.bPrint.Click += new System.EventHandler(this.bPrint_Click);
+            // 
+            // bTIF
+            // 
+            this.bTIF.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bTIF.Location = new System.Drawing.Point(168, 123);
+            this.bTIF.Name = "bTIF";
+            this.bTIF.Size = new System.Drawing.Size(150, 46);
+            this.bTIF.TabIndex = 4;
+            this.bTIF.Text = "&TIF";
+            this.bTIF.UseVisualStyleBackColor = true;
+            this.bTIF.Click += new System.EventHandler(this.bTIF_Click);
+            // 
+            // bPDF
+            // 
+            this.bPDF.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bPDF.Location = new System.Drawing.Point(324, 123);
+            this.bPDF.Name = "bPDF";
+            this.bPDF.Size = new System.Drawing.Size(150, 46);
+            this.bPDF.TabIndex = 5;
+            this.bPDF.Text = "&PDF";
+            this.bPDF.UseVisualStyleBackColor = true;
+            this.bPDF.Click += new System.EventHandler(this.bPDF_Click);
+            // 
+            // cbb
+            // 
+            this.cbb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbb.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbb.FormattingEnabled = true;
+            this.cbb.Location = new System.Drawing.Point(12, 67);
+            this.cbb.Name = "cbb";
+            this.cbb.Size = new System.Drawing.Size(462, 29);
+            this.cbb.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(498, 186);
+            this.Controls.Add(this.cbb);
+            this.Controls.Add(this.bPDF);
+            this.Controls.Add(this.bTIF);
+            this.Controls.Add(this.bPrint);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "バーコード印刷2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button bPrint;
+        private System.Windows.Forms.Button bTIF;
+        private System.Windows.Forms.Button bPDF;
+        private System.Windows.Forms.ComboBox cbb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing.OneD;
+
+namespace PrintBarCode2 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void bPrint_Click(object sender, EventArgs e) {
+            PrintDialog p = new PrintDialog();
+            p.UseEXDialog = true;
+            PrintDocument doc = new PrintDocument();
+            doc.DocumentName = NormName;
+            p.Document = doc;
+            if (p.ShowDialog(this) != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            doc.PrintPage += doc_PrintPage;
+
+            doc.Print();
+        }
+
+        bool[] al;
+
+        void doc_PrintPage(object sender, PrintPageEventArgs e) {
+            var cv = e.Graphics;
+
+            PrintIt(cv);
+
+            e.HasMorePages = false;
+        }
+
+        void PrintIt(Graphics cv) {
+            var w = cbb.SelectedItem as OneDimensionalCodeWriter;
+            if (w == null) return;
+            al = w.encode(tb.Text);
+
+            cv.PageUnit = GraphicsUnit.Document;
+            var rc = cv.VisibleClipBounds;
+
+            float w1 = 5, M = 100;
+            for (int x = 0; x < al.Length; x++) {
+                var rc1 = new RectangleF(
+                    M + w1 * x,
+                    M,
+                    w1,
+                    25 * w1
+                    );
+                cv.FillRectangle(al[x] ? Brushes.Black : Brushes.White, rc1);
+                var rc2 = rc1;
+                rc2.X = rc.Right - M - w1 * al.Length + w1 * x;
+                cv.FillRectangle(al[x] ? Brushes.Black : Brushes.White, rc2);
+            }
+        }
+
+        Bitmap Gen() {
+            var pic = new Bitmap(2480, 3504);
+            pic.SetResolution(300, 300);
+            using (var cv = Graphics.FromImage(pic)) {
+                cv.Clear(Color.White);
+
+                PrintIt(cv);
+                return pic;
+            }
+        }
+
+        private void bTIF_Click(object sender, EventArgs e) {
+            using (var pic = Gen()) {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.FileName = NormName + ".tif";
+                sfd.Filter = "*.tif;*.tiff|*.tif;*.tiff";
+                if (sfd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                    return;
+                ImageCodecInfo ici = ImageCodecInfo.GetImageEncoders().First(q => q.MimeType.Equals("image/tiff"));
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+                myEncoderParameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (Int64)EncoderValue.CompressionCCITT4);
+                pic.Save(sfd.FileName, ici, myEncoderParameters);
+            }
+        }
+
+        private void bPDF_Click(object sender, EventArgs e) {
+            using (var pic = Gen()) {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.FileName = NormName + ".pdf";
+                sfd.Filter = "*.pdf|*.pdf";
+                if (sfd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                    return;
+                String fp1 = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N") + ".tif");
+                ImageCodecInfo ici = ImageCodecInfo.GetImageEncoders().First(q => q.MimeType.Equals("image/tiff"));
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+                myEncoderParameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (Int64)EncoderValue.CompressionCCITT4);
+                pic.Save(fp1, ici, myEncoderParameters);
+                Process p = Process.Start(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tiff2pdf.exe"), " -o \"" + sfd.FileName + "\" \"" + fp1 + "\" ");
+                p.WaitForExit();
+                File.Delete(fp1);
+            }
+        }
+
+        public string NormName { get { return tb.Text.Replace("*", ""); } }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            cbb.Items.Add(new ZXing.OneD.Code39Writer());
+            cbb.Items.Add(new ZXing.OneD.Code128Writer());
+            cbb.Items.Add(new ZXing.OneD.CodaBarWriter());
+            cbb.Items.Add(new ZXing.OneD.EAN13Writer());
+            cbb.Items.Add(new ZXing.OneD.EAN8Writer());
+            //cbb.Items.Add(new ZXing.OneD.ITFWriter());
+            //cbb.Items.Add(new ZXing.OneD.MSIWriter());
+            //cbb.Items.Add(new ZXing.OneD.PlesseyWriter());
+            //cbb.Items.Add(new ZXing.OneD.UPCAWriter());
+            //cbb.Items.Add(new ZXing.Aztec.AztecWriter());
+            //cbb.Items.Add(new ZXing.Datamatrix.DataMatrixWriter());
+            //cbb.Items.Add(new ZXing.PDF417.PDF417Writer());
+            //cbb.Items.Add(new ZXing.QrCode.QRCodeWriter());
+
+            cbb.SelectedIndex = 0;
+        }
+    }
+}
